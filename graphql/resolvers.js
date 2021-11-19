d49cd219f3c502ad628cc63c8b78f895a66723b5
Jose David Salazar <jdsaja98@gmail.com>
2021-11-19 11:29:52 -0500

    Complemento proyecto

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,5 +1,6 @@
 
 import { resolversAvance } from '../models/advancement/resolversAdvance.js';
 import { resolversUsuario } from '../models/user/resolversUser.js';
+import { resolversProject } from '../models/project/resolversProject.js';
 
-export const resolvers = [ resolversUsuario, resolversAvance ];
+export const resolvers = [ resolversUsuario, resolversAvance, resolversProject ];
--- graphql/types.js
+++ graphql/types.js
@@ -2,11 +2,11 @@ import { gql } from 'apollo-server-express';
 import { tiposAvance } from '../models/advancement/typeAdvance.js';
 import { typesEnums } from '../models/enums/typesEnums.js';
 import { tiposUsuario } from '../models/user/typesUser.js';
-import { tiposProyecto } from '../models/project/typesProject.js';
+import { typesProject } from '../models/project/typesProject.js';
 
 
 const typesGlobales = gql`
   scalar Date
 `;
 
-export const types = [ typesGlobales, typesEnums, tiposUsuario, tiposAvance, tiposProyecto ];
+export const types = [ typesGlobales, typesEnums, tiposUsuario, tiposAvance, typesProject ];
